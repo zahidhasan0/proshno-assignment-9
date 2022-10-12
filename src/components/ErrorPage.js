9f018739975f138e0c3f1e945ec23d7d5efe8526
zahidhasan0 <zahid.business2002@gmail.com>
2022-10-12 21:29:46 +0600

    error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const error = useRouteError()
+    console.log(error)
     return (
-        <div className='text-center'>
-            <h3 className='text-3xl'>Sorry. Your Page is not found.</h3>
-            <h3 className='text-5xl'>404</h3>
+        <div className='text-center mt-60'>
+            <h3 className='text-3xl font-bold text-red-600'>Sorry. You have an error.</h3>
+            <h3 className='text-2xl my-4'> {error.statusText}</h3>
+            <h3 className='text-5xl'>{error.status}</h3>
         </div>
     );
 };
