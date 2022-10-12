08b4e1743769d00ab9705aff990657b619e4dc3c
zahidhasan0 <zahid.business2002@gmail.com>
2022-10-12 11:45:48 +0600

    active link

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -9,17 +9,17 @@ const Statistics = () => {
     return (
         <div>
             <h3 className='text-3xl text-sky-900 font-semibold border-b-2 w-2/3 mx-auto shadow-lg mt-7'>Line Chart Of Total Quizes Of Every Single Topic</h3>
-            <div className='md:w-2/3 mx-auto mt-9'>
+            <div className='md:w-1/2 mx-auto mt-9'>
 
-                <ResponsiveContainer height={`100%`} width={`100%`}>
-                    <LineChart width={600} height={600} data={allCourses} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                        <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                    </LineChart>
-                </ResponsiveContainer>
+
+                <LineChart width={300} height={300} data={allCourses} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+
 
 
 
